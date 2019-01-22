508710c09a4a9779094bfad7fc0ac85ee2f8183f
Nyagajrr <muchokijnrbenson@gmail.com>
2019-01-22 11:08:29 +0300

    importing components in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubFormComponent } from './github-form/github-form.component';
+import { LinkComponent } from './link/link.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+const routes:Routes=[
+  {path:"searches",component:AppComponent},
+  {path:"link",component:LinkComponent},
+  {path:"",redirectTo:"/searches",pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    GithubFormComponent
+    GithubFormComponent,
+    LinkComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
