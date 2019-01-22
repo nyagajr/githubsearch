cc8a502b293dc9c72e8c21d0612816d49e433b7a
Nyagajrr <muchokijnrbenson@gmail.com>
2019-01-22 11:17:12 +0300

    importing modules

---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule,Routes} from '@angular/router'
+
+import { GoalComponent } from '../goal/goal.component';
+import { AboutComponent } from '../about/about.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class RoutingModule { }
