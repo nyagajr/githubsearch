707fb48cdbeb5428e82b213cae9042a3e987fd75
Nyagajrr <muchokijnrbenson@gmail.com>
2019-01-22 11:09:24 +0300

    adding a search service

---
+++ src/app/git/search.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Gitsearch } from '../gitsearch';
+// import { AppComponent } from '../app'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  // search(){
+  //   return earches;
+  // }
+  constructor() { }
+}
