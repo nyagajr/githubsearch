649115421d8eb07da624aba3c031262911fcb8d8
Nyagajrr <muchokijnrbenson@gmail.com>
2019-01-24 16:05:25 +0300

    importing components in routing module

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule,Routes} from '@angular/router'
 
-// import { GoalComponent } from '../goal/goal.component';
-import { LinkComponent } from '../link/link.component';
-import { NotFoundComponent } from '../not-found/not-found.component';
+
 @NgModule({
   declarations: [],
   imports: [
